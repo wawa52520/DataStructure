814e5817eda1174eb199da3e89bce31058e0576f
Garben <877048038@qq.com>
2022-03-03 10:30:28 +0800

    LeetCode1 ---todo

--- src/LeetCode/Title1.java
+++ src/LeetCode/Title1.java
@@ -2,18 +2,24 @@ package LeetCode;
 
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * @description: 两数之和
+ * @author: yaojiabin
+ * @date: 2022/3/3 10:29
+ */
 public class Title1 {
     public static int[] twoSum(int[] nums, int target) {
-        int[] result = new int[1];
+        //返回的结果集
+        int[] result = new int[2];
+        //将传来的数据存入map
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0;i < nums.length ; i++){
             map.put(nums[i],i);
         }
         for (int i = 0; i < nums.length; i++) {
-            if (map.get(nums[i]) + nums[i+1] == target) {
+            if (map.containsKey(target-nums[i]) && (map.get(target - nums[i])) != i ) {
                 result[0] = i;
-                result[1] = i+1;
+                result[1] = map.get(target - nums[i]);
                 return result;
             }
         }
@@ -24,6 +30,8 @@ public class Title1 {
     public static void main(String[] args) {
         int[] test = new int[]{2,7,11,15};
         int[] sum = twoSum(test, 9);
-        System.out.println(sum);
+        for (int i : sum) {
+            System.out.println(i);
+        }
     }
 }
